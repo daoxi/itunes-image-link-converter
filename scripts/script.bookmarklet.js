4cc1501b2caf8a3f4007ec32a5ad98f7cfb76175
Daoxi Sun <daoxisun@gmail.com>
2022-05-30 02:39:11 -0400

    created the script for converting itunes image url using regex

--- scripts/script.bookmarklet.js
+++ scripts/script.bookmarklet.js
@@ -1,3 +1,12 @@
 javascript: (function () {
-  // code
+  /* Use regex to replace the original URL */
+  function convertUrl(url) {
+    const firstPart = "https://a1.mzstatic.com/us/r1000/0";
+    const regex = /\/Music.*\.jpg/;
+    const urlMatch = url.match(regex);
+    const newUrl = urlMatch ? `${firstPart}${urlMatch[0]}` : url;
+    return newUrl;
+  }
+  const newUrl = convertUrl(location.href);
+  window.open(newUrl, "_blank");
 })();
