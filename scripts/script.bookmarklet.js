20cdd227e100e6dee04b5f3172e9163f76492e89
Daoxi Sun <daoxisun@gmail.com>
2022-05-30 02:53:12 -0400

    Improved the regex logic to handle more situations

--- scripts/script.bookmarklet.js
+++ scripts/script.bookmarklet.js
@@ -2,7 +2,7 @@ javascript: (function () {
   /* Use regex to replace the original URL */
   function convertUrl(url) {
     const firstPart = "https://a1.mzstatic.com/us/r1000/0";
-    const regex = /\/Music.*\.jpg/;
+    const regex = /\/(Music|Features|Video).*\.(jpg|png|gif)/;
     const urlMatch = url.match(regex);
     const newUrl = urlMatch ? `${firstPart}${urlMatch[0]}` : url;
     return newUrl;
