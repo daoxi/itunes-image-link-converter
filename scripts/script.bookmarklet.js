a14e8a7472c51f57ef737a88066c44a33449a31e
Daoxi Sun <daoxisun@gmail.com>
2022-06-30 21:37:07 -0400

    Now uses corresponding index for the subdomain part of URL

--- scripts/script.bookmarklet.js
+++ scripts/script.bookmarklet.js
@@ -1,14 +1,18 @@
 javascript: (function () {
   /* Replace the original URL and go to the new URL */
   function convertUrl(url) {
-    const newUrlFirstPart = "https://a1.mzstatic.com/us/r1000/0/";
+    /* From the URL, get the subdomain index, which is a single character ranging from 1 to 5 */
+    const oldUrlIndex = url.split("://is")[1].substr(0, 1);
+    const newUrlFirstPart = `https://a${oldUrlIndex}.mzstatic.com/us/r1000/0/`;
+
     /* Get everything after the first occurring "/image/thumb/" in the original URL */
     const oldUrlLastPart = url.split("/image/thumb/")[1];
     /* Get everything before the last occuring "/" in the oldUrlLastPart */
     const newUrlLastPart = oldUrlLastPart
       ? oldUrlLastPart.slice(0, oldUrlLastPart.lastIndexOf("/"))
       : null;
-    /* concatenate both parts with template literals */
+
+    /* concatenate both parts */
     const newUrl = newUrlLastPart ? `${newUrlFirstPart}${newUrlLastPart}` : url;
     return newUrl;
   }
